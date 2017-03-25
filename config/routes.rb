2ee3d2ff61dac98ce8c2d19f5e947a5ab4d4371b
Joe <jcipoll1@binghamton.edu>
2017-03-25 18:17:17 +0000

    Little Refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'events#index'
   resources :events
   resources :users do
-    # Creates users/login(.:format)  users#index
+    # Creates users/login(.:format)  users#login
     collection {get 'login'}
   end
       
