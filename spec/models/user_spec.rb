83524e715377a4a7510f97c3109d6368954635fb
Shafkat Haque <shaque4@binghamton.edu>
2017-04-18 15:10:02 +0000

    added rpsec for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,6 +22,26 @@ describe User do
       expect(new_user.oauth_token).to eq("abcdefg12345")
       expect(new_user.oauth_expires_at.to_i).to eq((auth[:credentials][:expires_at]).to_i)
     end
+  
+  
+  
+    it "returns the user by id" do
+      auth = {
+        provider: "google",
+        uid: "12345678910",
+        info: {
+          name: "Jesse",
+        },
+        credentials: {
+          token: "abcdefg12345",
+          expires_at: DateTime.now
+        }
+      }
+      User.from_omniauth(auth)
+      User.from_omniauth(auth)
+      expect(User.from_omniauth(auth)).to eq(User.first)
+    end
+    
   end
 end
 
