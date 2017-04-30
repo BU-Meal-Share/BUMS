2c370f3995aa5d0b79ffc1471877513de95b29ff
Joe <jcipoll1@binghamton.edu>
2017-04-30 20:53:25 +0000

    Updating Rspec

--- spec/controllers/attendees_controller_spec.rb
+++ spec/controllers/attendees_controller_spec.rb
@@ -1,31 +1,44 @@
 require 'rails_helper'
 
 describe AttendeesController do
-  pending "add some examples to (or delete) #{__FILE__}"
-=begin
+  #pending "add some examples to (or delete) #{__FILE__}"
     context 'When logged in' do
         describe "#create" do
             #fixtures :users
-            fixtures :events
+            auth = {
+                provider: "google",
+                uid: "12345678910",
+                info: {
+                    name: "Jesse",
+                },
+                credentials: {
+                    token: "abcdefg12345",
+                    expires_at: DateTime.now
+                }
+            }
+            User.from_omniauth(auth)
+
+            #fixtures :events
             fixtures :attendees
             
             before :each do
-                @event = events(:vegan_potluck)
+                #@event = events(:vegan_potluck)
                 @id = 1
-                @user = double(:id => @id)
-                #@user = users(:user_jcipoll)
+                #@user = double(:id => @id)
+                @event = double(:id => @id)
+                @user = User.first
                 @attendee = attendees(:attendee_1)
-                
             end
             
-            let(:params) { {:user_id => @user.id, :event_id => @event.id} }
+            let(:params) { {:event_id => @event.id} }
             
             it "invokes model method to create" do
                 expect(Attendee).to receive(:create!).with(params).and_return(@attendee)
-                post :create, attendees: params
+                post :create, {:event_id => @event.id}, {:event_id => @event.id}
+                #post :create, {:event_id => @event.id}, {:user_id => @user.id, :event_id => @event.id}
+                #{:user_id => @user.id, :event_id => @event.id} #event_id: @event.id #attendees: params
                 
             end
         end
     end
-=end
 end
